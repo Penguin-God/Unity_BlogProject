3d41f12dde0b9b706800a8d899f18ad701dbe9fb
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2022-12-06 19:00:20 +0900

    unit 공속 개념 추가

--- CleanGameArchitecture/Assets/0_CleanColorRandomDefense/3_Controllers/UnitController.cs
+++ CleanGameArchitecture/Assets/0_CleanColorRandomDefense/3_Controllers/UnitController.cs
@@ -11,11 +11,13 @@ public class UnitController : MonoBehaviour, IPositionGetter
     Vector3 ChasePos => _target.PositionGetter.Position;
     public Vector3 Position => transform.position;
 
+    [SerializeField] float _speed = 15;
     NavMeshAgent _nav;
     UnitAttackUseCase _unitAttackUseCase;
     void Awake()
     {
         _nav = GetComponent<NavMeshAgent>();
+        _nav.speed = _speed;
     }
 
     public void SetInfo(UnitAttackUseCase unitAttackUseCase)
@@ -29,6 +31,7 @@ public class UnitController : MonoBehaviour, IPositionGetter
         _target = Managers.Game.Monster.FindProximateMonster(Position);
     }
 
+    [SerializeField] bool _attackable = true;
     void Update()
     {
         if(_target == null || _target.IsDead)
@@ -37,6 +40,17 @@ public class UnitController : MonoBehaviour, IPositionGetter
             return;
         }
         _nav.SetDestination(ChasePos);
-        _unitAttackUseCase.TryAttack(_target);
+        if (_attackable)
+        {
+            _unitAttackUseCase.TryAttack(_target);
+            _attackable = false;
+            StartCoroutine(Co_CoolDawnAttack(1.5f));
+        }
+    }
+
+    IEnumerator Co_CoolDawnAttack(float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        _attackable = true;
     }
 }
