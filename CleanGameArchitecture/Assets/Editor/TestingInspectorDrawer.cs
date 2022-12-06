e9b05d696f1fb54f9251ec72c8d414504b296022
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2022-12-06 20:01:39 +0900

    테스트 인스팩터 준비 작업

---
+++ CleanGameArchitecture/Assets/Editor/TestingInspectorDrawer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Reflection;
+
+[CustomEditor(typeof(Tester))]
+public class TestingInspectorDrawer : Editor
+{
+    Tester userSkillTest;
+    private void OnEnable()
+    {
+        userSkillTest = (Tester)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        
+    }
+
+    void DrawTestButtons()
+    {
+        // if (GUILayout.Button())
+    }
+}
